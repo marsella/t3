947752454d17bfcadd51345f57c3b4c97b3aec0b
Marcella Hastings <marcellahastings@gmail.com>
2013-10-26 10:28:25 -0400

    board + triangles

--- t3.elm
+++ t3.elm
@@ -1,5 +1,3 @@
--- tic tac toe
-
 import Mouse
 import Window
 
@@ -8,11 +6,24 @@ clickLocations = foldp (::) [] (sampleOn Mouse.clicks Mouse.position)
 clicks num =  text <| Text.height 50 <| toText <| show num
 
 drawBoard (w,h) = 
-  let drawVBar = rect 10 (toFloat h) |> filled (hsva 23.0 1 1 1.0)
-      path = segment (toFloat w / 6, -1 * toFloat h) (toFloat w / 6, toFloat h)
-          |> traced (solid (hsva 55 1 1 1.0))
+      --style : LineStyle
+  let style = { color=(hsva 23 1 2 1.0), width=10, cap=Round, join=Smooth, dashing=[], dashOffset=0 }
+  
+      path p1 p2 = 
+          segment p1 p2
+          |> traced style
+      vbars = path (toFloat w / 6,      -1 * toFloat h) 
+                   (toFloat w / 6,      toFloat h) :: 
+              path (-1 * toFloat w / 6, -1 * toFloat h) 
+                   (-1 * toFloat w / 6, toFloat h) :: 
+              []
+      hbars = path (toFloat w,      toFloat h / 6) 
+                   (-1 * toFloat w, toFloat h / 6) :: 
+              path (toFloat w,      -1 * toFloat h / 6) 
+                   (-1 * toFloat w, -1 * toFloat h / 6) :: 
+              []
 
-  in collage w h [path]
+  in collage w h <| vbars ++ hbars
 
 display (w,h) locs =
   let tri (x,y) = 
