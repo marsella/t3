28728bb6739f42d96cd66af20bb7878d25d03063
Marcella Hastings <marcellahastings@gmail.com>
2013-11-03 16:51:18 -0500

    Reduced some funkiness with the hardcoded size. Left some in, too.

--- t3.elm
+++ t3.elm
@@ -42,25 +42,25 @@ fromList spaces =
 
 -- Display
 
-drawSpace : (Int, Int) -> Space -> Element
-drawSpace (w,h) space = 
+drawSpace : Space -> Element
+drawSpace space = 
   let bigLetter l = text <| Text.height 150 <| toText l
-      dim x = round (toFloat x / 3)
-      cont s = container (dim w) (dim h) middle (bigLetter s)
+      dim x = round (x / 3)
+      cont s = container (dim dims) (dim dims) middle (bigLetter s)
   in case space of 
-    Nothing -> spacer (dim w) (dim h)
+    Nothing -> spacer (dim dims) (dim dims)
     Just X -> cont "x"
     Just O -> cont "o"
 
-drawPieces : (Int,Int) -> Board -> Element
-drawPieces dims {top, mid, bot} = 
-  let ds = drawSpace dims
+drawPieces : Board -> Element
+drawPieces {top, mid, bot} = 
+  let ds = drawSpace
       drawRow {l, m, r} = flow right [ds l, ds m, ds r]
   in flow down [drawRow top, drawRow mid, drawRow bot]
 
-drawBoard : (Int,Int) -> Game -> Element
-drawBoard (w,h) {p, board} = 
-  let style = { color=(hsva 18 1 1 1.0), 
+drawBoard : Game -> Element
+drawBoard {p, board} = 
+  let style = { color=(hsva 18 3 1 1.0), 
                 width=10, 
                 cap=Round, 
                 join=Smooth, 
@@ -68,22 +68,18 @@ drawBoard (w,h) {p, board} =
                 dashOffset=0 }
   
       path p1 p2 = segment p1 p2 |> traced style
-          
-      -- center of the window is (0,0). I don't want to talk about it.
-      vbars = [path (toFloat w / 6,      -1 * toFloat h) 
-                   (toFloat w / 6,      toFloat h),
-              path (-1 * toFloat w / 6, -1 * toFloat h) 
-                   (-1 * toFloat w / 6, toFloat h)]
+      w = dims
+      h = dims
+      -- This is heinous because paths work relative to the previous bit.
+      -- Will improve soon.
+      vbars = [path (w / 6, -1 * h)      (w / 6, h),
+               path (-1 * w / 6, -1 * h) (-1 * w / 6, h)]
               
-      hbars = [path (toFloat w,      toFloat h / 6) 
-                   (-1 * toFloat w, toFloat h / 6), 
-              path (toFloat w,      -1 * toFloat h / 6) 
-                   (-1 * toFloat w, -1 * toFloat h / 6)]
-      coords = asText (w,h)
+      hbars = [path (w, h / 6)      (-1 * w, h / 6), 
+               path (w, -1 * h / 6) (-1 * w, -1 * h / 6)]
 
-  in layers [ collage w h <| vbars ++ hbars,
-              coords,
-              drawPieces (w,h) board ]
+  in layers [ collage (round w) (round h) <| vbars ++ hbars,
+              drawPieces board ]
 
 -- Update
 detectBox : Input -> (Int,Int)
@@ -92,7 +88,7 @@ detectBox (Input (x,y)) =
         if | (toFloat l) < (g / 3) -> 1
            | (toFloat l) < (2 * g / 3) -> 2
            | (toFloat l) < g -> 3
-           | otherwise -> 4
+           | otherwise -> 4 --break stuff
       row = cond y dims
       col = cond x dims
   in (col, row)
@@ -108,9 +104,9 @@ updateBoard {p, board} input =
          | c == 2 -> {l=l, m=Just p, r=r}
          | c == 3 -> {l=l, m=m, r=Just p}
   in case (detectBox input) of 
-    (c, 1) -> {top=(placeRow c top), mid=mid, bot=bot}
-    (c, 2) -> {top=top, mid=placeRow c mid, bot=bot}
-    (c, 3) -> {top=top, mid=mid, bot=placeRow c bot}
+    (c, 1) -> {top=(placeRow c top), mid=mid,            bot=bot}
+    (c, 2) -> {top=top,              mid=placeRow c mid, bot=bot}
+    (c, 3) -> {top=top,              mid=mid,            bot=placeRow c bot}
     _  -> initialBoard
 
 fullBoard = 
@@ -138,4 +134,4 @@ stepGame input {p, board} =
 
 gameState = foldp stepGame initialGame inputs
 
-main = lift2 drawBoard Window.dimensions gameState
+main = lift drawBoard gameState
